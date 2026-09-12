85633475eff2145b6e8913f60f6ee20a8ca8036b
Shalini Choudhary <60210475+shalini47ch@users.noreply.github.com>
2026-09-12 16:59:12 +0530

    Time: 11 ms (25.97%), Space: 20.5 MB (18.96%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,13 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        #use cyclic sort to solve this 
-        n=len(nums)
+        #here we need to return the missing number
+        #lets use cyclic sort to solve this 
         i=0
-        #since it is from 0 till n so correct is nums[i] else it will be nums[i]-1
+        n=len(nums)
         while(i<n):
             correct=nums[i]
             if(nums[i]<n and nums[i]!=nums[correct]):
-                #perform the swap
+                #perform swap
                 nums[i],nums[correct]=nums[correct],nums[i]
             else:
                 i+=1
